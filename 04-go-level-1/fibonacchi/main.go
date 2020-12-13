788590c823ba0fecf42c0109fc1d2c6352490eaa
Evgeniy <n0fr1@mail.ru>
2020-12-13 23:29:14 +0300

    upd fibonacchi for 4 hw

--- 04-go-level-1/fibonacchi/main.go
+++ 04-go-level-1/fibonacchi/main.go
@@ -9,11 +9,10 @@ import (
 
 func main() {
 
-	var result uint64 = 0 //итоговый результат
-
-	mapArg := make(map[string]uint64) //мэп - где будет храниться два аргумента.
+	mapArg := make(map[string]uint64) //мэп - где будет храниться два аргумента и конечный результат
 	mapArg["a"] = 0
 	mapArg["b"] = 0
+	mapArg["result"] = 0
 
 	fmt.Print("Введите номер числа фибоначчи:\n")
 	input := bufio.NewScanner(os.Stdin)
@@ -21,33 +20,33 @@ func main() {
 
 		num, err := strconv.ParseUint(input.Text(), 10, 64)
 
-		if err != nil { //проверва на числовое значение
+		if err != nil { //проверка на числовое значение
 			fmt.Fprint(os.Stderr, "Вы ввели не число! \n")
 			os.Exit(1)
 		}
 
-		fibonacchi := count(num, result, mapArg)
+		fibonacchi := count(num, mapArg)
 		fmt.Printf("Число фибоначчи: %d", fibonacchi)
 
 	}
 
 }
 
-func count(num, result uint64, mapArg map[string]uint64) uint64 {
+func count(num uint64, mapArg map[string]uint64) uint64 {
 
 	if num == 0 { //если num = 0, выходим из рекурсии.
-		return result
+		return mapArg["result"]
 	}
 
-	if result == 0 && mapArg["a"] == 0 && mapArg["b"] == 0 { //вычисляем первый проход.
-		mapArg["b"] = 1 //при первом проходе а = 0, b = 1.
-		return count(num-1, 0, mapArg)
+	if mapArg["result"] == 0 && mapArg["a"] == 0 && mapArg["b"] == 0 { //ловим первый проход.
+		mapArg["b"] = 1 //при первом проходе result = 0, а = 0, b = 1.
+		return count(num-1, mapArg)
 	}
 
-	result = mapArg["a"] + mapArg["b"] //логика работы, пример: result = a(5) + b(3) = 8
-	mapArg["b"] = mapArg["a"]          //меняем местами. b(3) = a(5) , => b = 5
-	mapArg["a"] = result               //a(5) = result(8), a => 8. На следующем шаге: result = a(8)+ b(5)
+	mapArg["result"] = mapArg["a"] + mapArg["b"] //логика работы, пример: result = a(5) + b(3) = 8
+	mapArg["b"] = mapArg["a"]                    //меняем местами. b(3) = a(5) , => b = 5
+	mapArg["a"] = mapArg["result"]               //a(5) = result(8), a => 8. На следующем шаге: result = a(8)+ b(5)
 
-	return count(num-1, result, mapArg)
+	return count(num-1, mapArg)
 
 }
